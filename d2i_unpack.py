57c56218bbe5664caa64b0c7306853bb4127e7ca
Valentin Alix <valentin-alix.850@outlook.fr>
2024-06-06 11:12:08 +0200

    enclose in main

--- d2i_unpack.py
+++ d2i_unpack.py
@@ -8,17 +8,17 @@ from pydofus.d2i import D2I
 # python d2i_unpack.py file.d2i
 # file output: file.json
 
-path_input = os.path.join(DOFUS_PATH, "data", "i18n", "i18n_fr.d2i")
-path_output = os.path.join(Path(__file__).parent, "output", "d2i.json")
+if __name__ == "__main__":
+    path_input = os.path.join(DOFUS_PATH, "data", "i18n", "i18n_fr.d2i")
+    path_output = os.path.join(Path(__file__).parent, "output", "d2i.json")
 
+    d2i_input = open(path_input, "rb")
+    json_output = open(path_output, "w", encoding="utf-8")
 
-d2i_input = open(path_input, "rb")
-json_output = open(path_output, "w", encoding="utf-8")
+    d2i = D2I(d2i_input)
+    data = d2i.read()
 
-d2i = D2I(d2i_input)
-data = d2i.read()
+    json.dump(data, json_output, indent=2, ensure_ascii=False)
 
-json.dump(data, json_output, indent=2, ensure_ascii=False)
-
-d2i_input.close()
-json_output.close()
+    d2i_input.close()
+    json_output.close()
--- d2o_unpack.py
+++ d2o_unpack.py
@@ -8,24 +8,25 @@ from pydofus.d2o import D2OReader, InvalidD2OFile
 # python d2o_unpack.py (all files in input folder)
 # folder output: ./output/{all files}
 
-path_input = os.path.join(DOFUS_PATH, "data", "common\\")
-path_output = os.path.join(Path(__file__).parent, "output", "d2o\\")
+if __name__ == "__main__":
+    path_input = os.path.join(DOFUS_PATH, "data", "common\\")
+    path_output = os.path.join(Path(__file__).parent, "output", "d2o\\")
 
-os.makedirs(path_output, exist_ok=True)
+    os.makedirs(path_output, exist_ok=True)
 
-for file in os.listdir(path_input):
-    if file.endswith(".d2o"):
-        file_name = os.path.basename(file)
-        d2p_file = open(path_input + file, "rb")
+    for file in os.listdir(path_input):
+        if file.endswith(".d2o"):
+            file_name = os.path.basename(file)
+            d2p_file = open(path_input + file, "rb")
 
-        print("D2O Unpacker for " + file_name)
+            print("D2O Unpacker for " + file_name)
 
-        try:
-            d2o_reader = D2OReader(d2p_file)
-            d2o_data = d2o_reader.get_objects()
+            try:
+                d2o_reader = D2OReader(d2p_file)
+                d2o_data = d2o_reader.get_objects()
 
-            json_output = open(path_output + file_name.replace("d2o", "json"), "w")
-            json.dump(d2o_data, json_output, indent=4)
-            json_output.close()
-        except InvalidD2OFile:
-            pass
+                json_output = open(path_output + file_name.replace("d2o", "json"), "w")
+                json.dump(d2o_data, json_output, indent=4)
+                json_output.close()
+            except InvalidD2OFile:
+                pass
